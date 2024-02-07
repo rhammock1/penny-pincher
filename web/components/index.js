d6afd326b6a6b85c6aadd53f2276a02e2e44c0ef
rhammock1 <robert.hammock@gotab.io>
2024-02-07 18:45:05 -0500

    simplify component import

--- web/components/index.js
+++ web/components/index.js
@@ -1,31 +1,16 @@
 // ALL CUSTOM COMPONENTS MUST BE IMPORTED HERE
 
 // Dashboard Components
-import GraphCarousel from "./cards/graph-carousel.js";
-import LedgerOverview from "./cards/ledger-overview.js";
-import AccountBalances from "./cards/account-balances.js";
-import PieChart from "./charts/pie-chart.js";
-import LineChart from "./charts/line-chart.js";
-import TableChart from "./charts/table-chart.js";
+import "./cards/graph-carousel.js";
+import "./cards/ledger-overview.js";
+import "./cards/account-balances.js";
+import "./charts/pie-chart.js";
+import "./charts/line-chart.js";
+import "./charts/table-chart.js";
 
 // General/View Components
-import AppWrapper from "./app-wrapper.js";
-import ClassifyTransactions from "./cards/classify-transactions.js";
-import ServiceEnrollment from "./cards/service-enrollment.js";
-import FinancialGoals from "./cards/financial-goals/financial-goals.js";
-import NoData from "./no-data.js";
-
-// Export isn't necessary, but I don't like the unused variable warning
-export default {
-  GraphCarousel,
-  LedgerOverview,
-  PieChart,
-  LineChart,
-  AccountBalances,
-  TableChart,
-  AppWrapper,
-  ClassifyTransactions,
-  ServiceEnrollment,
-  FinancialGoals,
-  NoData,
-};
+import "./app-wrapper.js";
+import "./cards/classify-transactions.js";
+import "./cards/service-enrollment.js";
+import "./cards/financial-goals/financial-goals.js";
+import "./no-data.js";
