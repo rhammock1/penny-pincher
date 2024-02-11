4e4af1206f71679f73fdec1bae4044b12bb66fa1
rhammock1 <robert.hammock@gotab.io>
2024-02-11 11:34:07 -0500

    WIP goal form

---
+++ web/components/cards/financial-goals/components/add-goal-form.js
@@ -0,0 +1,76 @@
+import {fetcher} from '../../../../utils.js';
+
+export class AddGoalForm extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const {goal_types} = this.input;
+
+    this.innerHTML = `
+      <form id="add-goal-form">
+        <div class="form-group">
+          <label for="goal-name">Goal Name</label>
+          <input type="text" id="goal-name" name="name" required>
+          <label for="short-name">Short Name</label>
+          <input type="text" id="short-name" name="short_name" required>
+        </div>
+        <div class="form-group">
+          <label for="goal-amount">Goal Amount</label>
+          <input type="number" id="goal-amount" name="goal_amount" required>
+        </div>
+        <div class="form-group">
+          <label for="target-date">Goal Date</label>
+          <input type="date" id="target-date" name="target_date">
+        </div>
+        <div class="form-group">
+          <label for="goal-type">Goal Type</label>
+          <select id="goal-type" name="goal_type">
+            <option value="">Select a goal type</option>
+            ${goal_types?.map((type) => `<option value="${type}">${type}</option>`).join('')}
+          </select>
+        </div>
+        <button id="cancel-add-goal" type="button">Cancel</button>
+        <button id="submit-add-goal" type="submit">Submit</button>
+      </form>
+    `;
+
+    const form = document.getElementById('add-goal-form');
+    form.addEventListener('submit', this.handleSubmit.bind(this));
+
+    const cancel_btn = document.getElementById('cancel-add-goal');
+    cancel_btn.addEventListener('click', this.handleCancel.bind(this));
+  }
+
+  handleCancel(e) {
+    this.dispatchEvent(new CustomEvent('cancel-add-goal'));
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const goal_name = document.getElementById('goal-name').value;
+    const goal_amount = document.getElementById('goal-amount').value;
+    const goal_date = document.getElementById('target-date').value;
+    const short_name = document.getElementById('short-name').value;
+    const goal_type = document.getElementById('goal-type').value;
+
+    const goal = {
+      goal_name,
+      goal_amount,
+      goal_date,
+      short_name,
+      goal_type,
+    };
+
+    const response = await fetcher(this.input.request, 'POST', goal);
+
+    if(response.ok) {
+      this.dispatchEvent(new CustomEvent('added-goal', {detail: goal}));
+    }
+  }
+}
+
+customElements.define('add-goal-form', AddGoalForm);
--- web/components/cards/financial-goals/financial-goals.js
+++ web/components/cards/financial-goals/financial-goals.js
@@ -1,8 +1,8 @@
 import { fetcher } from "../../../utils.js";
 
 // Components
-// import './goal-list.js';
-// import './add-goal-form.js';
+import './components/goal-list.js';
+import './components/add-goal-form.js';
 
 /*
   The goal of this component is to recreate Digit to a degree.
@@ -38,21 +38,27 @@ export default class FinancialGoals extends HTMLElement {
     `;
 
     const goals = document.getElementById('financial-goals-body');
+
+    const add_goal_btn = document.createElement('button');
+    add_goal_btn.setAttribute('id', 'add-goal');
+    add_goal_btn.innerHTML = 'Add Goal';
     fetcher(request)
       .then(res => (res ? res.json() : {}))
-      .then(({data = []}) => {
-        if(!data.length) {
-          goals.innerHTML = `
-            <no-data>Please add a goal to get started.</no-data>
-              <button id="add-goal">Add Goal</button>
-          `;
+      .then(({data = {}}) => {
+        if(!data?.goals?.length) {
+          const no_data = document.createElement('no-data');
+          no_data.innerHTML = `Please add a goal to get started.`;
+          goals.innerHTML = '';
+          goals.appendChild(no_data);
+          goals.appendChild(add_goal_btn);
         } else {
           const goal_list = document.createElement('goal-list');
           goal_list.input = {
-            data,
+            goals: data?.goals,
           };
           goals.innerHTML = ``;
           goals.appendChild(goal_list);
+          goals.appendChild(add_goal_btn);
         }
         return data
       })
@@ -64,11 +70,32 @@ export default class FinancialGoals extends HTMLElement {
 
           const form = document.createElement('add-goal-form');
           form.input = {
-            data,
+            request,
+            goal_types: data.goal_types,
           };
           goals.innerHTML = '';
           goals.appendChild(form);
-        })
+
+          form.addEventListener('added-goal', (e) => {
+            const { goal } = e.detail;
+            this.state.goals.push(goal);
+          });
+          form.addEventListener('cancel-add-goal', (e) => {
+            goals.innerHTML = '';
+            if(!data?.goals?.length) {
+              const no_data = document.createElement('no-data');
+              no_data.innerHTML = `Please add a goal to get started.`;
+              goals.appendChild(no_data);
+            } else {
+              const goal_list = document.createElement('goal-list');
+              goal_list.input = {
+                goals: data?.goals,
+              };
+              goals.appendChild(goal_list);
+            }
+            goals.appendChild(add_goal_btn);
+          });
+        });
       })
       .catch(err => {
         console.error(err);
